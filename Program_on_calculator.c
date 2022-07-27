3c61c534f08c9127c5e8493b623c4231ea5378c0
RizvikumariCH <95178942+RizvikumariCH@users.noreply.github.com>
2022-07-27 20:47:44 +0530

    Commands are given

--- Program_on_calculator.c
+++ Program_on_calculator.c
@@ -3,3 +3,7 @@
 int main(){
   char Operator;
   float p, q, result = 0;
+  printf("\n Enter any one operator like +,-,*,/:");
+  scanf("%c",&Operator);
+  printf("Enter the values of Operators p and q\n:");
+  scanf("%f%f",&p,&q);

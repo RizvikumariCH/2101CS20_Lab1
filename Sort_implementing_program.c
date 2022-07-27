da105c4ab5a97939ab9a57cfbb0b8c6ea97bd362
RizvikumariCH <95178942+RizvikumariCH@users.noreply.github.com>
2022-07-27 21:25:24 +0530

    Bubble sort function is defined

--- Sort_implementing_program.c
+++ Sort_implementing_program.c
@@ -60,4 +60,23 @@ void display(int arr[],int n)
     printf("%d",arr[i]);
   }
 }
+//Bubble Sort Function//
+void bubble_sort(int arr[],int n)
+{
+  int i,j,temp;
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<n-i-1;j++)
+    {
+      if(arr[j]>arr[j+1])
+      {
+        temp=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=temp;
+      }
+    }
+  }
+  printf("After Bubble sort elements are :");
+  display(arr,n);
+}
   

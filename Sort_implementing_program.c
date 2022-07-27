188fa4b47056980b339149ae90641df2776109d7
RizvikumariCH <95178942+RizvikumariCH@users.noreply.github.com>
2022-07-27 21:10:54 +0530

    Types of sorts are defined

---
+++ Sort_implementing_program.c
@@ -0,0 +1,26 @@
+//program implementing different sorts//
+#include<stdio.h>
+void display(int a[],int n);
+void bubble_sort(int a[],int n);
+void selection_sort(int a[],int n);
+void insertion_sort(int a[],int n);
+void quick_sort(int a[],int n);
+void merge_sort(int a[],int n);
+int main()
+{
+  int n,choice,i;
+  char ch[20];
+  printf("Enter no.of elements you want to sort:");
+  scanf("%d",&n);
+  int arr[n];
+  for(i=0;i<n;i++)
+  }
+printf("Enter %d element:",i+1);
+scanf("%d",&arr[i]);
+}
+while(1)
+{
+  printf("\n1. Bubble Sort\n2. Selection Sort\n3. Insertion Sort\n4. Quick Sort\n5. Merge Sort\n6. Display Array\n7. Exit the Program\n");
+  printf("\nEnter your choice:");
+  scanf("%d",&choice);
+  
